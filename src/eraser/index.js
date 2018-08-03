cdb9a00265f943be92c03285077855bb1160c4a6
hamger <2313303800@qq.com>
2018-08-03 17:52:09 +0800

    fix

--- src/eraser/index.js
+++ src/eraser/index.js
@@ -1,22 +1,15 @@
 import './index.scss'
 import Eraser from './eraser.js'
 
-var eraser1 = new Eraser({
-  container: document.getElementById('aa'),
-  frontImg: require('../assets/eraser1.jpg'),
-  backImg: require('../assets/eraser2.jpg'),
+var eraser = new Eraser({
+  container: document.getElementById('container'),
+  frontImg: require('../assets/eraser2.jpg'),
+  backImg: require('../assets/eraser1.jpg'),
   callback: function () {
     console.log('擦完了')
   }
 })
 
-var eraser2 = new Eraser({
-  container: document.getElementById('bb'),
-  frontImg: require('../assets/eraser2.jpg'),
-  backImg: require('../assets/eraser1.jpg')
-})
-
 document.getElementById('reset').addEventListener('click', () => {
-  eraser1.reset()
-  eraser2.reset()
+  eraser.reset()
 })
