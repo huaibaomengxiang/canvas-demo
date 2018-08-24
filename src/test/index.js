68798d7062445917dd076b0633385dc25349c650
hamger <2313303800@qq.com>
2018-08-24 14:57:20 +0800

    add see readme

--- src/test/index.js
+++ src/test/index.js
@@ -1,13 +1,15 @@
-import {Canvas, Circle} from '../../see'
+import { Canvas, Circle } from '../../see'
 
 var canvas = new Canvas({
   container: document.getElementById('container')
 })
 
-canvas.addChild(new Circle({
-  x: 100,
-  y: 100,
-  r: 59
-}))
+canvas.addChild(
+  new Circle({
+    x: 100,
+    y: 100,
+    r: 59
+  })
+)
 
 canvas.draw()
